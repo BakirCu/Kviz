15d0ef1519f5ee941d94cc402fd1ba6fcba121da
Bakir <bakir.feniks@gmail.com>
2020-04-23 17:47:46 +0200

    Napravio tabele, Kviz, Pitanje i Odgovor

--- kvizer/models.py
+++ kvizer/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
-# Create your models here.
+
+class Kviz(models.Model):
+    PREDMETI = (("Matematika", "Matematika"),
+                ("Fizika", "Fizika"),
+                ("Srpski jezik i književnost", "Srpski jezik i književnost"),
+                ("Srpski kao nematernji jezik", "Srpski kao nematernji"),
+                ("Bosanski jezik i književnost", "Bosanski jezik i književnost"),
+                ("Engleski jezik", "Engleski jezik"),
+                ("Nemački jezik", "Nemački jezik"),
+                ("Francuski jezik", "Francuski jezik"),
+                ("Latinski jezik", "Latinski jezik"),
+                ("Istorija", "Istorija"),
+                ("Geografija", "Geografija"),
+                ("Biologija", "Biologija"),
+                ("Hemija", "Hemija"),
+                ("Računarstvo i informatika", "Računarstvo i informatika"),
+                ("Muzička kultura", "Muzička kultura"),
+                ("Likovna kultura", "Likovna kultura"),
+                ("Fizičko vaspitanje", "Fizičko vaspitanje"),
+                ("Građansko vaspitanje", "Građansko vaspitanje"),
+                ("Islamska veronauka", "Islamska veronauka"),
+                ("Pravoslavna veronauka", "Pravoslavna veronauka"),
+                ("Jezik medija i kultura", "Jezik medija i kultura"),
+                ("Primenjene nauke", "Primenjene nauke"),
+                ("Umetnost i dizajn", "Umetnost i dizajn"),
+                ("Zdravlje i sport", "Zdravlje i sport"),
+                ("Psihologija", "Psihologija"),
+                ("Filozofija", "Filozofija"),
+                ("Sociologija", "Sociologija"),
+                ("Ustav i prava građana", "Ustav i prava građana"),
+                )
+
+    GODINE = (("Prva godina", "Prva godina"),
+              ("Druga godina", "Druga godina"),
+              ("Treća godina", "Treća godina"),
+              ("Četvrta godina", "Četvrta godina"), )
+    naziv = models.CharField(max_length=300)
+    predmet = models.CharField(choices=PREDMETI, max_length=40)
+    godina = models.CharField(choices=GODINE, max_length=40)
+    id_korisnika = models.PositiveIntegerField()
+    trajanje_kviza = models.PositiveIntegerField(
+        help_text='Trajanje kviza je u minutama')
+
+    def __str__(self):
+        return f'{self.naziv}'
+
+
+class Pitanje(models.Model):
+    id_kviza = models.ForeignKey(Kviz, on_delete=models.CASCADE)
+    text = models.TextField(max_length=300)
+
+    def __str__(self):
+        return f'{self.id_kviza}'
+
+
+class Odgovor(models.Model):
+    id_pitanja = models.ForeignKey(Pitanje, on_delete=models.CASCADE)
+    tacnost = models.BooleanField()
+
+    def __str__(self):
+        return f'{self.id_pitanja}'
