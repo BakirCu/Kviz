39e2637eb90496823fc21d8ffb9e764617142a37
Bakir <bakir.feniks@gmail.com>
2020-04-25 02:23:31 +0200

    Napravio stranicu za odgovore i pitanja

--- kvizer/urls.py
+++ kvizer/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.create_kviz, name='create_kviz'),
-
+    path('answers/', views.create_answers, name='create_answers'),
 ]
--- kvizer/views.py
+++ kvizer/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def create_kviz(request):
     return render(request, 'kvizer/create_kviz.html')
+
+
+def create_answers(request):
+    return render(request, 'kvizer/create_answers.html')
